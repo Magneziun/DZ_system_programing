6f585076a8c20964ba6104e02d428e61aaee1dc3
Magnezium <Teram1o@yandex.ru>
2026-02-16 23:00:10 +0500

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Model;
+
+public class Product
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public int Pages { get; set; }           
+    public decimal Price { get; set; }
+    public DateTime PublishDate { get; set; }     
+    public int AuthorId { get; set; }         
+    public int ThemeId { get; set; }               
+
+    public override string ToString()
+    {
+        return $"Id: {Id}, Name: {Name}, Pages: {Pages}, Price: {Price}, PublishDate: {PublishDate.ToShortDateString()}, AuthorId: {AuthorId}, ThemeId: {ThemeId}";
+    }
+}
